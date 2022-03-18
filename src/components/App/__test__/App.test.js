767a87f92af1047a954b03c0d86e12f1304a1e63
kmquirapas <kmquirapas@gmail.com>
2022-03-18 11:02:10 +0800

    final push with tests working

--- src/components/App/__test__/App.test.js
+++ src/components/App/__test__/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
+import { AuthProvider } from '../../../contexts/AuthContext';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders App', () => {
+  render(
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  );
 });
